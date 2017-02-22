eec65e749ea0703e424cb4dbcc61187308637459
samsolomon <samuelrobertsolomon@gmail.com>
2017-02-22 16:14:21 -0500

    change home vh

--- homepage.php
+++ homepage.php
@@ -13,7 +13,7 @@ get_header(); ?>
 			<h1 class="">Sam Solomon is a casual <a href="#">writer</a> and professional product <a href="#">designer</a>.</h1>
 			<p class="mt40">I  work at <a href="#">Trustfuel</a> and host the DN FM podcast.  I write about topics at the intersection of business and design. If that's your thing, consider subscribing.</p>
 			<h4 class="mt20">Join Hundreds of Good-Looking People</h4>
-			<form class="form-inline mt20 mb-vh-15">
+			<form class="form-inline mt20 mb-vh-25">
 			  <div class="form-group">
 			    <input type="text" class="form-control min-width350" id="example" placeholder="Your email Address…">
 			  </div>
