904f32e5e4f2e4a0f004dc9b012734bbd51c2356
samsolomon <samuelrobertsolomon@gmail.com>
2017-03-07 09:41:11 -0500

    add blockquote

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -13,40 +13,40 @@
 		</div>
 
 		<div class="row mt30">
-			<div class="col-sm-7">
+			<div class="col-sm-6">
 
-		<div class="entry-content">
-			<?php
-				the_content();
-
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'solomon-v3' ),
-					'after'  => '</div>',
-				) );
-			?>
-		</div><!-- .entry-content -->
-
-		<?php if ( get_edit_post_link() ) : ?>
-			<footer class="entry-footer">
+			<div class="entry-content">
 				<?php
-					edit_post_link(
-						sprintf(
-							/* translators: %s: Name of current post */
-							esc_html__( 'Edit %s', 'solomon-v3' ),
-							the_title( '<span class="screen-reader-text">"', '"</span>', false )
-						),
-						'<span class="edit-link">',
-						'</span>'
-					);
-				?>
-			</footer><!-- .entry-footer -->
+					the_content();
 
-		</div><!-- end column -->
-		<div class="col-sm-offset-2 col-sm-3">
-			<h4 class="mb10"><?php $nav_menu = wp_get_nav_menu_object(21); echo $nav_menu->name; ?></h4>
-			<?php wp_nav_menu( array( 'theme_location' => 'footer-right-menu', 'container_class' => 'footer-menu' ) ); ?>
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'solomon-v3' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+
+			<?php if ( get_edit_post_link() ) : ?>
+				<footer class="entry-footer">
+					<?php
+						edit_post_link(
+							sprintf(
+								/* translators: %s: Name of current post */
+								esc_html__( 'Edit %s', 'solomon-v3' ),
+								the_title( '<span class="screen-reader-text">"', '"</span>', false )
+							),
+							'<span class="edit-link">',
+							'</span>'
+						);
+					?>
+				</footer><!-- .entry-footer -->
+
+			</div><!-- end column -->
+			<div class="col-sm-offset-2 col-sm-3">
+				<h4 class="mb10"><?php $nav_menu = wp_get_nav_menu_object(21); echo $nav_menu->name; ?></h4>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-right-menu', 'container_class' => 'footer-menu' ) ); ?>
+				</div>
 			</div>
-		</div>
 
 		</div>
 
