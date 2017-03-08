1c0e167bfb99bfd3baca775d158bce4d3f640acd
samsolomon <samuelrobertsolomon@gmail.com>
2017-03-08 11:04:52 -0500

    comment progress

--- comments.php
+++ comments.php
@@ -15,71 +15,120 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
-	return;
-}
-?>
+ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && 'comments.php' == basename( $_SERVER['SCRIPT_FILENAME'] ) )
+ 	die ( 'Please do not load this page directly. Thanks!' );
 
-<div id="comments" class="comments-area">
+ if ( post_password_required() ) {
+ 	?>
+ 	<p class="nocomments"><?php _e( 'This post is password protected. Enter the password to view comments.', 'okay' ); ?></p>
+ 	<?php
+ 	return;
+ }
+ ?>
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'solomon-v3' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			?>
-		</h2><!-- .comments-title -->
+<div id="comments">
+  <div class="container">
+  	<h3 id="comments-title">
+  		<?php comments_number( __( 'Leave A Comment', 'okay' ), __( '1 Comment', 'okay' ), __( '% Comments', 'okay' ) ); ?>
+  	</h3>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'solomon-v3' ); ?></h2>
-			<div class="nav-links">
+  <h3 id="comments-title">
+    <?php
+      printf( // WPCS: XSS OK.
+        esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'solomon-v3' ) ),
+        number_format_i18n( get_comments_number() ),
+        '<span>' . get_the_title() . '</span>'
+      );
+    ?>
+  </h3><!-- .comments-title -->
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'solomon-v3' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'solomon-v3' ) ); ?></div>
+  	<div class="comments-wrap">
+  		<ol class="commentlist">
+      <?php
+        wp_list_comments( array(
+          'style'      => 'ol',
+          'short_ping' => true,
+        ) );
+      ?>
+  		</ol>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
+  		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+  			<nav id="comment-nav-below" role="navigation">
+  				<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'okay' ) ); ?></div>
+  				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'okay' ) ); ?></div>
+  			</nav>
+  		<?php endif; // check for comment navigation ?>
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
-			?>
-		</ol><!-- .comment-list -->
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'solomon-v3' ); ?></h2>
-			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'solomon-v3' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'solomon-v3' ) ); ?></div>
-
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
-		<?php
-		endif; // Check for comment navigation.
-
-	endif; // Check for have_comments().
-
-
-	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'solomon-v3' ); ?></p>
-	<?php
-	endif;
-
-	comment_form();
-	?>
-
+  		<?php comment_form(); ?>
+  	</div>
+  	<!-- .comments-wrap -->
+  </div><!-- .conatiner -->
 </div><!-- #comments -->
+
+
+<!-- *********************************** -->
+
+
+ <div id="comments" class="comments-area">
+
+ 	<?php
+ 	// You can start editing here -- including this comment!
+ 	if ( have_comments() ) : ?>
+ 		<h3 id="comments-title">
+ 			<?php
+ 				printf( // WPCS: XSS OK.
+ 					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'solomon-v3' ) ),
+ 					number_format_i18n( get_comments_number() ),
+ 					'<span>' . get_the_title() . '</span>'
+ 				);
+ 			?>
+ 		</h3><!-- .comments-title -->
+
+ 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+ 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+ 			<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'solomon-v3' ); ?></h3>
+ 			<div class="nav-links">
+
+ 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'solomon-v3' ) ); ?></div>
+ 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'solomon-v3' ) ); ?></div>
+
+ 			</div><!-- .nav-links -->
+ 		</nav><!-- #comment-nav-above -->
+ 		<?php endif; // Check for comment navigation. ?>
+
+ 		<ol class="comment-list">
+ 			<?php
+ 				wp_list_comments( array(
+ 					'style'      => 'ol',
+ 					'short_ping' => true,
+ 				) );
+ 			?>
+ 		</ol><!-- .comment-list -->
+
+ 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+ 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+ 			<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'solomon-v3' ); ?></h3>
+ 			<div class="nav-links">
+
+ 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'solomon-v3' ) ); ?></div>
+ 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'solomon-v3' ) ); ?></div>
+
+ 			</div><!-- .nav-links -->
+ 		</nav><!-- #comment-nav-below -->
+ 		<?php
+ 		endif; // Check for comment navigation.
+
+ 	endif; // Check for have_comments().
+
+
+ 	// If comments are closed and there are comments, let's leave a little note, shall we?
+ 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+
+ 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'solomon-v3' ); ?></p>
+ 	<?php
+ 	endif;
+
+ 	comment_form();
+ 	?>
+
+ </div><!-- #comments -->
--- functions.php
+++ functions.php
@@ -118,6 +118,8 @@ function solomon_v3_scripts() {
 
 	wp_enqueue_script( 'solomon-v3-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'solomon-v3-comments', get_template_directory_uri() . '/js/comments.js', array(), '20151215', true );
+
 	wp_enqueue_script( 'solomon-v3-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
