51d0b4f9b16b50b4237b481667f56d9d6726f086
samsolomon <samuelrobertsolomon@gmail.com>
2017-03-09 11:16:04 -0500

    remove prev next post nav

--- single.php
+++ single.php
@@ -52,7 +52,7 @@
 
 			get_template_part( 'template-parts/content', 'post' );
 
-			the_post_navigation();
+			// the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
