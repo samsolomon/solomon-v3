f934313e70b6a582fdc012a592d35248a21413c3
samsolomon <samuelrobertsolomon@gmail.com>
2017-03-10 11:10:16 -0500

    suggested post styling

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -41,6 +41,44 @@
 		</div>
 	</div>
 
+	<!-- suggested posts -->
+	<?php
+		// check if the repeater field has rows of data
+		if( have_rows('suggested_posts') ): ?>
+			<section class="container-post suggested-posts">
+			<h4 class="text-center">Suggested Reading</h4>
+			<?php
+			// loop through the rows of data
+			while ( have_rows('suggested_posts') ) : the_row(); ?>
+				<?php
+				$post_object = get_sub_field('suggested_post');
+				if( $post_object ):
+					// override $post
+					$post = $post_object;
+					setup_postdata( $post );
+					?>
+				    <div class="mb50">
+				    	<h3 class="mb20 text-center"><a href="<?php the_permalink(); ?>" class="remove-underline"><?php the_title(); ?></a></h3>
+							<h4 class="mb20 text-center"><?php echo get_the_date(); ?> / <?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?></h4>
+							<?php the_excerpt(); ?>
+							<!-- <div class="author-information mt2">
+								<?php echo get_avatar( get_the_author_meta( 'ID' ), 36 ); ?>
+								<span class="ml1">by <span class="semibold"><?php the_author_posts_link(); ?></span> on <?php the_date(); ?></span>
+							</div> -->
+							<p class="text-center"><a href="<?php the_permalink();?>" class="color-primary">Continue Reading</a></p>
+				    </div>
+				    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+				<?php endif; ?>
+
+			<?php endwhile;
+			else :
+				// no rows found
+		endif;
+	?>
+	</section>
+	<!-- end suggested posts -->
+
+
 	<footer class="entry-footer">
 		<?php solomon_v3_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
